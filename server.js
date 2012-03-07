fa6940b73ea24796e8e70ca506e20ebfef79320b
Alexandru Scvortov <scvalex@gmail.com>
2012-03-07 23:01:44 +0000

    modularize code

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 "use strict";
 
-var http = require("http");
-var listenPort = 8888;
+function start(listenPort) {
+  var http = require("http");
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello, World");
-  response.end();
-}).listen(listenPort);
+  http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello, World");
+    response.end();
+  }).listen(listenPort);
 
-console.log("Server started on ", listenPort);
+  console.log("Server started on ", listenPort);
+}
+
+exports.start = start;
